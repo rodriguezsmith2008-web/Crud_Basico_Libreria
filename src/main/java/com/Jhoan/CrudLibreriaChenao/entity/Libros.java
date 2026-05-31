b0500f9fff633e963077248bf5cb45ad4bc585b0
rodriguezsmith2008-web <rodriguez.smith.2008@gmail.com>
2026-05-31 15:39:31 -0500

    feat:s Se crea la entidad reflejada en la base de datos con sus enum separados para mas orden

---
+++ src/main/java/com/Jhoan/CrudLibreriaChenao/entity/Libros.java
@@ -0,0 +1,34 @@
+package com.Jhoan.CrudLibreriaChenao.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "libros")
+public class Libros {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false, length = 100)
+    private String titulo;
+
+    @Column(name = "autor", nullable = false, length = 200)
+    private String autor;
+
+    @Column(name = "fecha_publicacion", nullable = false)
+    private LocalDate fechaPublicacion;
+
+    @Column(name = "estado")
+    private Long estado;
+}
